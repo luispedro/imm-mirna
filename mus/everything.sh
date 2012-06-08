52f30d4776280859100104e8c08e06d1db1563a3
Daniel Neves <danielneves2005@gmail.com>
2012-06-08 09:18:54 +0100

    added scripts

--- mus/everything.sh
+++ mus/everything.sh
@@ -1,20 +1,133 @@
 #!/bin/sh
 
-CMD_TAPYR="../tapyr13beta/tapyr"
+##############################################################
 
-REF_PREFILTERED="Mus_musculus.NCBIM37.67.ncrna.fa"
-REF_FILTERED_BASENAME="Mus_musculus.NCBIM37.67.ncrna.filtered"
+# TAPyR executable
+CMD_TAPYR="tapyr13beta/tapyr"
 
-READS1="Control-3T3_Clean.fq.trimmed.single"
-READS2="Stimulated_3T3_Clean.fq.trimmed.single"
+# FastQC executable
+CMD_FASTQC="FastQC/fastqc"
 
-cleanup_reference() {
-	cat $REF_PREFILTERED | python cleanup.py
+# DynamicTrim executable (http://solexaqa.sourceforge.net/)
+CMD_DYNAMICTRIM="solexaqa/DynamicTrim.pl"
+
+# LengthSort executable (http://solexaqa.sourceforge.net/)
+CMD_LENGTHSORT="solexaqa/LengthSort.pl"
+
+# Mus Musculus genome reference
+REF="ref/Mus_musculus.NCBIM37.67.ncrna.fa"
+
+# Reads files (FQ)
+READS1="Control-3T3.fq"
+READS2="Stimulated_3T3.fq"
+
+##############################################################
+
+REF_FILTERED_BASENAME="Genome.filtered"
+
+READS1_FILTERED=$READS1.filtered.fq
+READS2_FILTERED=$READS2.filtered.fq
+
+READS1_TRIMMED=$READS1_FILTERED.trimmed
+READS2_TRIMMED=$READS2_FILTERED.trimmed
+
+READS1_SORTED=$READS1_TRIMMED.single
+READS2_SORTED=$READS2_TRIMMED.single
+
+READS1_SAM=$READS1_TRIMMED.sam
+READS2_SAM=$READS2_TRIMMED.sam
+
+__preprocess_read() {
+	rfile=$1
+	rfile_filtered=$2
+
+	echo "Generating FastQC report for "$rfile
+	$CMD_FASTQC $rfile --noextract
+	
+	# get list of overrepresented sequences
+	echo "Removing known overrepresented sequences from "$rfile
+	overrep=$(unzip -p ${rfile}_fastqc.zip ${rfile}_fastqc/fastqc_data.txt | sed -n "/Overrepresented/,/>>END_MODULE/p" | head -n-1 | tail -n+3 | grep -v "No Hit")
+	seqs=$(echo "$overrep" | cut -f1 | tr '\n' '|' | sed s/.$//)
+	escseqs=$(echo $seqs | sed 's/|/\\|/g')
+	sed -n "h;n; /$escseqs/!{x;p;x;p;n;p;n;p;}; /$escseqs/{n;n;}" $rfile > $rfile_filtered
+
+	# tests
+	echo "Testing..."
+	target=$(echo "$overrep" | awk '{ sum += $2; } END { print sum; }')
+	n1=$(wc -l $rfile | cut -f1 -d' ')
+	n2=$(wc -l $rfile_filtered | cut -f1 -d' ')
+	echo "$( expr $(expr $n1 - $n2) / 4 ) of $target unwanted entries removed."
+	echo $(egrep "$seqs" $rfile_filtered | wc -l)" unwanted entries still present."
 }
-cleanup_reference > $REF_FILTERED_BASENAME.fa
 
-# Generate TAPyR index -> $REF_FILTERED_BASENAME.fmi
-$CMD_TAPYR I $REF_FILTERED_BASENAME.fa
+##############################################################
 
-# Align reads 100%
-$CMD_TAPYR $REF_FILTERED_BASENAME.fmi $READS1 -i 100
+# Preprocess reads files by running FastQC on them and removing
+# known overrepresented sequencesi
+#	-> $READS1_FILTERED
+#	-> $READS2_FILTERED
+preprocess_reads() {
+	__preprocess_read $READS1 $READS1_FILTERED
+	__preprocess_read $READS2 $READS2_FILTERED
+}
+
+# Trim sequences in reads files based on phred score. 
+# Sequences will be trimmed to the longest continuous segment
+# for which all bases have at minimum the phred score provided.
+#	-> $READS1_TRIMMED
+#	-> $READS2_TRIMMED
+trim_reads() {
+	phred=$1 	# Minimum phred score
+
+	echo "Trimming sequences. Phred = "$phred
+	$CMD_DYNAMICTRIM $READS1_FILTERED -h $phred
+	$CMD_DYNAMICTRIM $READS2_FILTERED -h $phred
+}
+
+# Discard sequences smaller than the minimum length provided.
+#	-> $READS1_SORTED
+#	-> $READS2_SORTED
+sort_discard_reads() {
+	length=$1	# Minimum sequence length
+
+	echo "Sorting sequences and discarding < "$length"bp"
+	$CMD_LENGTHSORT $READS1_TRIMMED -l $length
+	$CMD_LENGTHSORT $READS2_TRIMMED -l $length
+}
+
+# Cleanup reference genome by removing non-microRNA sequences
+#	-> $REF_FILTERED_BASENAME.fa
+cleanup_reference() {
+	echo "Removing non-microRNA entries from reference."
+	cat $REF | sed -n ':i /miRNA/{p; :a n; /^>/!{p;ba}; /^>/{bi} }' > $REF_FILTERED_BASENAME.fa 
+}
+
+# Generate TAPyR index 
+#	-> $REF_FILTERED_BASENAME.fmi
+generate_index() {
+	echo "Generating TAPyR index."
+	$CMD_TAPYR I $REF_FILTERED_BASENAME.fa
+}
+
+# Align reads
+#	-> $READS1.sam
+#	-> $READS2.sam
+align_reads() {
+	precision=$1	# Requested alignment precision (100 = perfect alignement)
+
+	echo "Aligning reads..."
+	$CMD_TAPYR $REF_FILTERED_BASENAME.fmi $READS1_SORTED -i $precision
+	$CMD_TAPYR $REF_FILTERED_BASENAME.fmi $READS2_SORTED -i $precision
+}
+
+##############################################################
+
+preprocess_reads
+trim_reads 30
+sort_discard_reads 16
+
+cleanup_reference 
+generate_index
+
+align_reads 100
+
--- tryp/everything.sh
+++ tryp/everything.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#######################################################################
+
 # Trypanosoma Brucei Genome (Required)
 # Chromosomes from http://www.genedb.org/Homepage/Tbruceibrucei927
 REF_SOURCE1="ref/Tb927_01_v4.embl"
@@ -21,45 +23,70 @@ CMD_TAPYR="/home/daniel/tapyr13beta/tapyr"
 READS1_TXT="50HA_tab_dl_prefilter.txt"
 READS2_TXT="51TY1_tab_dl_prefilter.txt"
 
-REF_BASENAME="ref/Tb927"
+#######################################################################
+
+REF_BASENAME="Tb927"
 
 # Create fasta from embl chromossomes.
-#
-# Generates: 
-# 	$REF_BASENAME.fa 
+# 	-> $REF_BASENAME.fa
 create_fasta_from_embl() {
-	#echo ">Tb927 Trypanosoma Brucei"
-	for i in 1 2 3 4 5 6 7 8 9 10 11 ; do
-		echo ">Tb927 Trypanosoma Brucei (Chromosome "$i")"
-		cat $(eval echo "\${REF_SOURCE$i}") | egrep "^  " | awk '{ for (i=1; i< NF; ++i) printf("%s", $i); printf("\n") }' 
-	done
-}
-#create_fasta_from_embl > $REF_BASENAME.fa
+	echo "Creating FASTA from EMBL chromosomes."
 
-# Create TAPyR index 
-#
-# Generates:
-#       $REF_BASENAME.fmi
-#$CMD_TAPYR I $REF_BASENAME.fa
+	(for i in 1 2 3 4 5 6 7 8 9 10 11 ; do
+		echo ">Tb927 Trypanosoma Brucei (Chromosome "$i")"
+		cat $(eval echo "\${REF_SOURCE$i}") | egrep "^  " | awk '{ for (i=1; i< NF; ++i) printf("%s", $i); printf("\n") }'
+	done) > $REF_BASENAME.fa
+}
+
+# Create TAPyR index
+# 	-> $REF_BASENAME.fmi
+create_index() {
+	echo "Creating TAPyR index."
+
+	$CMD_TAPYR I $REF_BASENAME.fa
+}
 
 # Convert reads files to fasta
-#
-# Generates: 
-#	$READS1_TXT.fa
-#	$READS2_TXT.fa
+# 	-> $READS1_TXT.fa
+#	-> $READS2_TXT.fa
 convert_reads_txt_to_fasta() {
+	echo "Converting reads files to FASTA"
+
 	cat $READS1_TXT | awk '{ printf(">%s\n%s\n", $1, $2) }' > $READS1_TXT.fa
 	cat $READS2_TXT | awk '{ printf(">%s\n%s\n", $1, $2) }' > $READS2_TXT.fa
 }
-#convert_reads_txt_to_fasta
 
-# Map reads
-#
-# Generates:
-#	$READS1_TXT.sam
-#$CMD_TAPYR $REF_BASENAME.fmi $READS1_TXT.fa -i 94
-#$CMD_TAPYR $REF_BASENAME.fmi $READS2_TXT.fa -i 94
+# Align reads 
+#	-> $READS1_TXT.sam
+#	-> $READS2_TXT.sam
+align_reads_94() {
+	precision=$1 	# Requested alignment precision (100 = perfect alignment)
 
-# Generate counts and concensus
-#$CMD_TAPYR c $REF_BASENAME.fa $READS1_TXT.sam
+	echo "Aligning reads."
+
+	$CMD_TAPYR $REF_BASENAME.fmi $READS1_TXT.fa -i $precision
+	$CMD_TAPYR $REF_BASENAME.fmi $READS2_TXT.fa -i $precision
+}
+
+# Generate counts
+#	->
+generate_counts() {
+	echo "Generating counts."
+
+	$CMD_TAPYR c $REF_BASENAME.fa $READS1_TXT.sam
+	mkdir ${READS1_TXT}_Counts
+	mv *-counts.txt *-fwd_counts.txt *-rev_counts.txt ${READS1_TXT}_Counts
+
+	$CMD_TAPYR c $REF_BASENAME.fa $READS2_TXT.sam
+	mkdir ${READS2_TXT}_Counts
+	mv *-counts.txt *-fwd_counts.txt *-rev_counts.txt ${READS2_TXT}_Counts
+}
+
+#######################################################################
+
+create_fasta_from_embl
+create_index
+convert_reads_txt_to_fasta
+align_reads 94
+generate_counts
 
